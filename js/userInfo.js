cca577a87767200285633d6e1d847e6207dd568e
Keerthana Anandan <58841274+KeerthanaAnandan@users.noreply.github.com>
2021-04-24 22:33:49 +0530

    updated

--- js/userInfo.js
+++ js/userInfo.js
@@ -12,13 +12,203 @@ let eleventhTypePoints = sessionStorage.getItem("eleventh");
 
 
 
-/*let one = document.getElementsByClassName("").innerHTML;
+
+
+document.querySelector(".one").innerHTML = firstTypePoints;
+document.querySelector(".two").innerHTML = secondTypePoints;
+document.querySelector(".three").innerHTML = thirdTypePoints;
+document.querySelector(".four").innerHTML = fourthTypePoints;
+document.querySelector(".five").innerHTML = fifthTypePoints;
+document.querySelector(".six").innerHTML = sixthTypePoints;
+document.querySelector(".seven").innerHTML = seventhTypePoints;
+document.querySelector(".eight").innerHTML = eighthTypePoints;
+document.querySelector(".nine").innerHTML = ninthTypePoints;
+document.querySelector(".ten").innerHTML = tenthTypePoints;
+document.querySelector(".eleven").innerHTML = eleventhTypePoints;
+
+let paraone;
+let paratwo;
+//first
+if(firstTypePoints <= 3){
+  paraone = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(firstTypePoints >=4 && firstTypePoints <= 6){
+  paraone = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(firstTypePoints >= 7 && firstTypePoints <= 9){
+  paraone = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(firstTypePoints == 10){
+paraone = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//second
+if(secondTypePoints <= 3){
+  paratwo = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(secondTypePoints >=4 && secondTypePoints <= 6){
+  paratwo = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(secondTypePoints >= 7 && secondTypePoints <= 9){
+  paratwo = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(secondTypePoints == 10){
+paratwo = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//third
+if(thirdTypePoints <= 3){
+  parathree = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(thirdTypePoints >=4 && thirdTypePoints <= 6){
+  parathree = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(thirdTypePoints >= 7 && thirdTypePoints <= 9){
+  parathree = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(thirdTypePoints == 10){
+parathree = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//four
+if(fourthTypePoints <= 3){
+  parafour = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(fourthTypePoints >=4 && fourthTypePoints <= 6){
+  parafour = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(fourthTypePoints >= 7 && fourthTypePoints <= 9){
+  parafour = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(fourthTypePoints == 10){
+parafour = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//five
+if(fifthTypePoints <= 3){
+  parafive = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(fifthTypePoints >=4 && fifthTypePoints <= 6){
+  parafive = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(fifthTypePoints >= 7 && fifthTypePoints <= 9){
+  parafive = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(fifthTypePoints == 10){
+parafive = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//six
+if(sixthTypePoints <= 3){
+  parasix = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(sixthTypePoints >=4 && sixthTypePoints <= 6){
+  parasix = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(sixthTypePoints >= 7 && sixthTypePoints <= 9){
+  parasix = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(sixthTypePoints == 10){
+parasix = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//seven
+if(seventhTypePoints <= 3){
+  paraseven = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(seventhTypePoints >=4 && seventhTypePoints <= 6){
+  paraseven = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(seventhTypePoints >= 7 && seventhTypePoints <= 9){
+  paraseven = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(seventhTypePoints == 10){
+paraseven = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//eight
+if(eighthTypePoints <= 3){
+  paraeight = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(eighthTypePoints >=4 && eighthTypePoints <= 6){
+  paraeight = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(eighthTypePoints >= 7 && eighthTypePoints <= 9){
+  paraeight = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(eighthTypePoints == 10){
+paraeight = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//snine
+if(ninthTypePoints <= 3){
+  paranine = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(ninthTypePoints >=4 && ninthTypePoints <= 6){
+  paranine = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(ninthTypePoints >= 7 && ninthTypePoints <= 9){
+  paranine = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(ninthTypePoints == 10){
+paranine = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//ten
+if(tenthTypePoints <= 3){
+  paraten = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(tenthTypePoints >=4 && tenthTypePoints <= 6){
+  paraten = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(tenthTypePoints >= 7 && tenthTypePoints <= 9){
+  paraten = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(tenthTypePoints == 10){
+paraten = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+//eleven
+if(eleventhTypePoints <= 3){
+  paraeleven = "Looks like you’re not quite happy in this area of your life, but the good news is that we can change that."
+}
+else if(eleventhTypePoints >=4 && eleventhTypePoints <= 6){
+  paraeleven = "This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you."
+}
+else if(eleventhTypePoints >= 7 && eleventhTypePoints <= 9){
+  paraeleven = "Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. "
+}
+else if(eleventhTypePoints == 10){
+paraeleven = "Congratulations - you’re living at your max in this area of your life - amazing!"
+}
+
+document.querySelector(".paraone").innerHTML = paraone;
+document.querySelector(".paratwo").innerHTML = paratwo;
+document.querySelector(".parathree").innerHTML = parathree;
+document.querySelector(".parafour").innerHTML = parafour;
+document.querySelector(".parafive").innerHTML = parafive;
+document.querySelector(".parasix").innerHTML = parasix;
+document.querySelector(".paraseven").innerHTML = paraseven;
+document.querySelector(".paraeight").innerHTML = paraeight;
+document.querySelector(".paranine").innerHTML = paranine;
+document.querySelector(".paraten").innerHTML = paraten;
+document.querySelector(".paraeleven").innerHTML = paraeleven;
+
+
+/*CUSTOM PARAGRAPH - SCORE 1-3
+Looks like you’re not quite happy in this area of your life, but the good news is that we can change that.
+
+
+CUSTOM PARAGRAPH - SCORE 4-6
+This area looks like it could use some love, but guess what -  I’m happy to jump on a FREE discovery call with you.
+
+
+CUSTOM PARAGRAPH - SCORE 7-9
+Above average! You are definitely scoring higher than others in this area of your life, but there’s still room for improvement. 
+
+
+
+CUSTOM PARAGRAPH - SCORE 10
+Congratulations - you’re living at your max in this area of your life - amazing!
+
+
+let one = document.getElementsByClassName("").innerHTML;
 timrff = document.getElementsByClassName("tick").style.display ="none";
 console.log(timrff)
-console.log(one)*/
+console.log(one)
 let mess;
 let typo;
-typo = "How Happy Are You?"
+typo = ""
 mess = [`1.Health & Vitality-${firstTypePoints}/10` , `2.Mindset & Emotional Mastery  - ${secondTypePoints + "/10"}`, `3.Romantic Love  - ${thirdTypePoints + "/10"}` , `4.Family   - ${fourthTypePoints + "/10"}` , `5.Friends & Social  - ${fifthTypePoints + "/10"}`,`6.Productivity & Performance  - ${sixthTypePoints + "/10"}`,`7.Career & Business  - ${seventhTypePoints + "/10"}`,`8.Wealth & Finances  - ${eighthTypePoints + "/10"}`,`9. Recreation & Lifestyle  - ${ninthTypePoints + "/10"}` ,`10.Leadership, Impact & Contribution   - ${tenthTypePoints + "/10"}`, `11. Spirituality   - ${eleventhTypePoints + "/10"}`]
 
 
@@ -29,7 +219,7 @@ const msgme = document.querySelector(".msghere");
 msgme.classList.add("center-item");
 //document.querySelector(".msghere").innerHTML = mao;
 
-msgme.innerHTML = mao;
+msgme.innerHTML = mao;*/
 //document.getElementsByClassName("tick").style.display = "none";
 //document.querySelector(".usertype").value = typo; 
 
